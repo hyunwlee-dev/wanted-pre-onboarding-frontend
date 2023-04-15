0e61b03cec1fb0a814adfb415c3adc71da09818b
hyunwlee-dev <hwlee3928@gmail.com>
2023-04-15 12:49:39 +0900

    :sparkles: FEATURE | add localstorage save, load, delete util functions

---
+++ src/utils/storage.js
@@ -0,0 +1,24 @@
+const {
+  localStorage: storage,
+  JSON: {stringify: serialize, parse: deserialize}
+} = globalThis;
+
+export function saveStorage(key, value) {
+  return new Promise((resolve) => {
+    storage.setItem(key, serialize(value));
+  });
+}
+
+export function loadStorage(key) {
+  return new Promise((resolve) => {
+    resolve(deserialize(storage.getItem(key)));
+  });
+}
+
+export function deleteStorage(key) {
+  return new Promise((resolve) => {
+    if (!key) storage.clear();
+    else storage.removeItem(key);
+    resolve();
+  });
+}
