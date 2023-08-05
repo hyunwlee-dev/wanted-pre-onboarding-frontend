c94b98c158532030e192039960bf824eaec4d769
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-05 22:01:37 +0900

    chore: styling Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,9 @@ import classes from './footer.module.css';
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      푸터
+      <span>Contact hyunwlee</span>
+      <span>Email: hwlee3928@gmail.com</span>
+      <a target='_blank' href='https://github.com/hyunwlee-dev'>github.hyunwlee-dev</a>
     </footer>
   )
 }
